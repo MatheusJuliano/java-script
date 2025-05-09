3d2ddaf711625eb68baab61335e05379d3c1c7aa
Matheus Juliano Da Silva Dos Santos <matheusjuliano243@gmail.com>
2025-05-09 02:24:42 -0300

    novos exercícios de java-script

---
+++ exercicos/ex008/aula1/index.js
@@ -0,0 +1,6 @@
+var vel = 12.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel >60){
+    console.log(`Voçe utrapassou o imite de velocidade, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ exercicos/ex008/aula1/index2.js
@@ -0,0 +1,7 @@
+var pais = `Brasi`
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('voçe é Brasileiro!')
+} else{
+    console.log('voçe é Estrengeiro!')
+}
